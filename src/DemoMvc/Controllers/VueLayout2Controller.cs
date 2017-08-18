8f95fed9acb3ee03cf54d6feaaa9ebfeabcb96b6
congzw <46074987@qq.com>
2017-08-18 10:47:40 +0800

    add v-for demo

---
+++ src/DemoMvc/Controllers/VueLayout2Controller.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class VueLayout2Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
