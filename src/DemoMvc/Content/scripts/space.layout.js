29eeddc07bda16754cf2ca6d42c8153d3a4332ae
congzw <46074987@qq.com>
2017-08-21 15:19:27 +0800

    fix demo

---
+++ src/DemoMvc/Content/scripts/space.layout.js
@@ -0,0 +1,19 @@
+﻿define('layoutVm', ['vue', 'ELEMENT'], function (Vue, ELEMENT) {
+
+    //全局注册饿了么组件
+    Vue.use(ELEMENT);
+
+    //全局注册自定义组件（DEMO）
+    Vue.component('nb-demo-layout', {
+        props: ['text'],
+        template: '<div>Shared Component: {{text}}</div>'
+    });
+    
+    var layoutVm = new Vue({
+        el: '#layout',
+        data: {
+            breadcrumbItems: []
+        }
+    });
+    return layoutVm;
+});
