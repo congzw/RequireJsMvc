4cad3889c2feb28bbf88b2c3a064df562409a080
congzw <46074987@qq.com>
2017-08-23 10:28:16 +0800

    add Tree Demo

---
+++ src/DemoMvc/Content/scripts/demo.layout.js
@@ -0,0 +1,53 @@
+﻿define('layoutVm', ['vue', 'ELEMENT'], function (Vue, ELEMENT) {
+
+    console.log('from layout define');
+    //全局注册饿了么组件
+    Vue.use(ELEMENT);
+
+    //全局注册自定义组件（DEMO）
+    Vue.component('nb-demo-layout', {
+        props: ['text'],
+        template: '<div>Shared Component: {{text}}</div>'
+    });
+
+    var layoutVm = new Vue({
+        el: '#layout',
+        data: {
+            breadcrumbItems: []
+            , demoTree: {
+                name: 'DEMO TREE',
+                children: [
+                  { name: 'hello' },
+                  { name: 'wat' },
+                  {
+                      name: 'child folder',
+                      children: [
+                        {
+                            name: 'child folder',
+                            children: [
+                              { name: 'hello' },
+                              { name: 'wat' }
+                            ]
+                        },
+                        { name: 'hello' },
+                        { name: 'wat' },
+                        {
+                            name: 'child folder',
+                            children: [
+                              { name: 'hello' },
+                              { name: 'wat' }
+                            ]
+                        }
+                      ]
+                  }
+                ]
+            }
+        }
+    });
+    return layoutVm;
+});
+
+require(['layoutVm'], function (layoutVm) {
+
+    console.log('from layout require');    console.log(layoutVm);
+});
---
+++ src/DemoMvc/Controllers/VueDemoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class VueDemoController : Controller
+    {
+        public ActionResult Tree()
+        {
+            return View();
+        }
+    }
+}
